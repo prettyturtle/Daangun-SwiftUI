ad85b58e915233b8dfcd548e8183b6704f45bb80
prettyturtle <unifconti@gmail.com>
2023-04-27 23:09:28 +0900

    Initial Commit

---
+++ Daangun/DaangunApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaangunApp.swift
+//  Daangun
+//
+//  Created by yc on 2023/04/27.
+//
+
+import SwiftUI
+
+@main
+struct DaangunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
